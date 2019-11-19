71544638ce5747fe9b7c15a859d7488273cb4b6c
Owen Voke <owzie123@gmail.com>
2019-11-19 15:01:49 +0000

    Add initial command template for 'download'

---
+++ app/Commands/DownloadCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Commands;
+
+use LaravelZero\Framework\Commands\Command;
+
+class DownloadCommand extends Command
+{
+    /** {@inheritdoc} */
+    protected $signature = 'download { trakt-user : The Trakt username for the list }
+                                     { --key= : Your Trakt API key (defaults to use the `TRAKT_API_KEY` environment variable if not provided) }
+                                     { --l|list=watchlist : A custom list id or stub }
+                                     { --o|output=./torrents : The directory to output data to }
+                                     { --quality= : The quality to download (720p, 1080p or 3D) }
+                                     { --statistics : Display download statistics }';
+
+    /** {@inheritdoc} */
+    protected $description = 'Download the contents of a Trakt list from YTS';
+
+    public function handle(): void
+    {
+    }
+}
