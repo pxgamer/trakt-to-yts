6ae06274df9266ea5ae66035d703983f34c89ae0
Owen Voke <owzie123@gmail.com>
2019-11-19 15:11:04 +0000

    Update descriptions

--- app/Commands/DownloadCommand.php
+++ app/Commands/DownloadCommand.php
@@ -7,8 +7,8 @@ use LaravelZero\Framework\Commands\Command;
 class DownloadCommand extends Command
 {
     /** {@inheritdoc} */
-    protected $signature = 'download { trakt-user : The Trakt username for the list }
-                                     { --key= : Your Trakt API key (defaults to use the `TRAKT_API_KEY` environment variable if not provided) }
+    protected $signature = 'download { trakt-user : Trakt username for the list }
+                                     { --key= : Trakt API key (defaults to use the `TRAKT_API_KEY` environment variable if not provided) }
                                      { --l|list=watchlist : A custom list id or stub }
                                      { --o|output=./torrents : The directory to output data to }
                                      { --quality= : The quality to download (720p, 1080p or 3D) }
