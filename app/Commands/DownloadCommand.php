6c07a723f06772554d35759c45f421be4cf9473e
Owen Voke <owzie123@gmail.com>
2019-11-19 17:40:06 +0000

    Add initial content for the Download command

--- app/Commands/DownloadCommand.php
+++ app/Commands/DownloadCommand.php
@@ -2,13 +2,15 @@
 
 namespace App\Commands;
 
+use App\Services\YTS\YtsApi;
+use App\Services\Trakt\TraktApi;
+use App\Services\Trakt\TraktMovie;
 use LaravelZero\Framework\Commands\Command;
 
 class DownloadCommand extends Command
 {
     /** {@inheritdoc} */
     protected $signature = 'download { trakt-user : Trakt username for the list }
-                                     { --key= : Trakt API key (defaults to use the `TRAKT_API_KEY` environment variable if not provided) }
                                      { --l|list=watchlist : A custom list id or stub }
                                      { --o|output=./torrents : The directory to output data to }
                                      { --quality= : The quality to download (720p, 1080p or 3D) }
@@ -17,7 +19,47 @@ class DownloadCommand extends Command
     /** {@inheritdoc} */
     protected $description = 'Download the contents of a Trakt list from YTS';
 
+    /** @var array<TraktMovie> */
+    private $traktList;
+
     public function handle(): void
     {
+        $this->retrieveTraktList();
+
+        $this->downloadTorrentsFromYts();
+    }
+
+    private function retrieveTraktList(): void
+    {
+        /** @var TraktApi $traktApi */
+        $traktApi = app(TraktApi::class);
+
+        $this->traktList = $traktApi->getList($this->argument('trakt-user'), $this->option('list'));
+
+        $this->comment("<options=bold>{$this->option('list')}</> (<options=bold>{$this->argument('trakt-user')}</>): Retrieved successfully");
+        $this->line(null);
+    }
+
+    private function downloadTorrentsFromYts(): void
+    {
+        /** @var YtsApi $ytsApi */
+        $ytsApi = app(YtsApi::class);
+
+        /** @var TraktMovie $movie */
+        foreach ($this->traktList as $movie) {
+            if (! $movie->getImdbId()) {
+                continue;
+            }
+
+            if (! $ytsListing = $ytsApi->getMovieByImdbId($movie->getImdbId())) {
+                $this->warn("'{$movie->getTitle()} ({$movie->getYear()})': Not found on YTS");
+                continue;
+            }
+
+            if ($ytsListing->getTorrents()->isEmpty()) {
+                $this->warn("'{$movie->getTitle()} ({$movie->getYear()})': No torrents available");
+                continue;
+            }
+        }
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,20 @@
 
 namespace App\Providers;
 
+use App\Services\Trakt\TraktApi;
+use App\Exceptions\ApiKeyNotSpecified;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function boot(): void
-    {
-        //
-    }
-
     public function register(): void
     {
-        //
+        $this->app->singleton(TraktApi::class, static function (): TraktApi {
+            if (! $traktApiKey = env('TRAKT_API_KEY')) {
+                throw ApiKeyNotSpecified::forServiceWithId(TraktApi::SERVICE_ID);
+            }
+
+            return new TraktApi($traktApiKey);
+        });
     }
 }
